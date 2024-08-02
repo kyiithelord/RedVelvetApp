b3c19e2846280aeb4315d63d9bb93f102546acb3
Lynn-Gilang <thetzinkyaw9727@gmail.com>
2024-08-02 23:27:44 +0630

    category_update

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <title>Document</title>
+</head>
+<body>
+        <div class="container max-w-3xl mx-auto p-3 mt-4">
+
+
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Id
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Description
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ( $categories as $category )
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{ $loop->iteration}}
+                </th>
+                <td class="px-6 py-4">
+                    {{$category->name}}
+                </td>
+                <td class="px-6 py-4">
+                    {{$category->description}}
+                </td>
+                <td class="px-6 py-4">
+                    <a href="{{route('category.edit',$category->id)}}" type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Edit</a>
+                    <form class="inline-block" action="{{route('category.destroy',$category->id)}}" method="POST">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+
+        </tbody>
+    </table>
+</div>
+
+        </div>
+
+
+
+
+</body>
+</html>
