6f9120866ff5077868c2e7342466a808d423c617
kyiithelord <thetzinkyaw9727@gmail.com>
2024-09-15 08:51:55 +0630

    confirm box to delete added

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -96,7 +96,7 @@
                 </td>
                 <td class="px-6 py-4">
                     <a href="{{route('category.edit',$category->id)}}" type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Edit</a>
-                    <form class="inline-block" action="{{route('category.destroy',$category->id)}}" method="POST">
+                    <form class="inline-block" action="{{route('category.destroy',$category->id)}}" onsubmit="return confirmDelete()"method="POST">
                         @csrf
                         @method('delete')
                         <button type="submit" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
@@ -108,10 +108,13 @@
         </tbody>
     </table>
 </div>
+</div>
 
-        </div>
-
-
+<script>
+    function confirmDelete(){
+        return confirm('Are you sure to delete')
+    }
+</script>
 
 
 </body>
